78eb901898e9c01ca3c30ab5444c102fb95ddce7
crowplexus <45212377+crowplexus@users.noreply.github.com>
2023-10-06 03:20:51 -0400

    edit doc scripts so they compile properly

--- docs/generate.bat
+++ docs/generate.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ..
-lime build hl --haxeflag="-xml docs/doc.xml"
+lime build windows -debug --haxeflag="-xml docs/doc.xml"
 haxelib run dox -i docs -o docs/pages --title "Forever Engine Documentation" --include external --include funkin --include forever
--- docs/generate.sh
+++ docs/generate.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 cd ..
-lime build hl --haxeflag="-xml docs/doc.xml"
+lime build linux -debug --haxeflag="-xml docs/doc.xml"
 haxelib run dox -i docs -o docs/pages --title "Forever Engine Documentation" --include external --include funkin --include forever
