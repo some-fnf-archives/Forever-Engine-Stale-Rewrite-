fa57b95be60497c68d764e17756853b41eea0929
crowplexus <45212377+crowplexus@users.noreply.github.com>
2023-11-16 13:02:43 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ haxelib install lime
 haxelib install openfl
 haxelib install hxdiscord_rpc
 haxelib install tjson
-haxelib git flixel-nova https://github.com/swordcube/flixel
-haxelib git flixel-addons-nova https://github.com/swordcube/flixel-addons
+haxelib git flixel-arwen https://github.com/crowplexus/flixel
+haxelib git flixel-addons-arwen https://github.com/crowplexus/flixel-addons
 haxelib git hscript-iris https://github.com/crowplexus/hscript-iris
 haxelib git yaml https://github.com/crowplexus/hx-yaml
 echo done.
