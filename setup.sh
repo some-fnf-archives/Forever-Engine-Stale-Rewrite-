74a7ba39341048db0b240a98ff85e750eac47037
crowplexus <lucariothelast91@gmail.com>
2023-11-23 14:06:12 -0400

    fix install scripts

--- setup.sh
+++ setup.sh
@@ -2,9 +2,13 @@
 echo Installing dependencies.
 haxelib install lime
 haxelib install openfl
+haxelib install flixel
+haxelib install flixel-addons
 haxelib install hxdiscord_rpc
 haxelib git flixel-arwen https://github.com/crowplexus/flixel
 haxelib git flixel-addons-arwen https://github.com/crowplexus/flixel-addons
 haxelib git hscript-iris https://github.com/crowplexus/hscript-iris
 haxelib git yaml https://github.com/crowplexus/hx-yaml
-echo done.
+echo Setting up Lime
+haxelib run lime setup
+echo Done!
